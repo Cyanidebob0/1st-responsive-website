d99f59939bb721d2f6f69cbf59ecf70821c88537
bhuvan <bhuvanannappa@gmail.com>
2024-09-11 11:18:06 +0530

    added mouse follower and fixed a bug with the scroll trigger

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-Shery.makeMagnet(".magnet-target .magnet", {
-    
-    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    duration: 1,
-  });
 
+let main=document.querySelector("#main");
+let cursor=document.querySelector("#cursor");
+
+main.addEventListener("mousemove",(e)=>{
+    gsap.to(cursor,{
+        x: e.x, 
+        y: e.y,
+        duration : 0.5
+    })
+})
+
+
+gsap.to(".fleftelm", {
+    scrollTrigger: {
+      trigger: ".f-images",
+      pin: true,
+      start: "top top",
+      end: "bottom bottom",
+      endTrigger: ".last",
+      scrub: 1,
+    },
+    y: "-300%",
+    ease: Power1,
+  });
+  
+  
