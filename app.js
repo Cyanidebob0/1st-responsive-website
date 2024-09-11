50d37b5c213634bf1acc3cd066bd4f77dcb4e416
bhuvan <bhuvanannappa@gmail.com>
2024-09-11 22:19:25 +0530

    made a hover over obj to play video feature

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 
 let main=document.querySelector("#main");
 let cursor=document.querySelector("#cursor");
+let hero=document.querySelectorAll(".overlay");
+let cBgCcolor=document.querySelector(".hvr")
 
-main.addEventListener("mousemove",(e)=>{
-    gsap.to(cursor,{
-        x: e.x, 
-        y: e.y,
-        duration : 0.7
-    })
-})
+ main.addEventListener("mousemove",(e)=>{
+     gsap.to(cursor,{
+         x: e.x, 
+         y: e.y,
+         duration : 1
+     })
+ })
 
 
 gsap.to(".fleftelm", {
@@ -25,6 +27,26 @@ gsap.to(".fleftelm", {
     ease: Power1,
   });
 
+
+ hero.forEach((ele)=>{
+    ele.addEventListener("mouseenter",()=>{
+      gsap.to(cursor,{
+        scale:8,
+      })
+    })
+ })
+
+ hero.forEach((ele)=>{
+  ele.addEventListener("mouseleave",()=>{
+    cBgCcolor.style.color="";  
+    gsap.to(cursor,{
+        scale:1,
+      })
+  })
+})
+
  
+
+
   
   
