9dd9638601a1364aceaf03da64fd5b186035ceba
bhuvan <bhuvanannappa@gmail.com>
2024-09-11 16:52:57 +0530

    WORKING

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ main.addEventListener("mousemove",(e)=>{
     gsap.to(cursor,{
         x: e.x, 
         y: e.y,
-        duration : 0.5
+        duration : 0.7
     })
 })
 
@@ -19,9 +19,12 @@ gsap.to(".fleftelm", {
       end: "bottom bottom",
       endTrigger: ".last",
       scrub: 1,
+      invalidateOnRefresh: true,
     },
     y: "-300%",
     ease: Power1,
   });
+
+ 
   
   
