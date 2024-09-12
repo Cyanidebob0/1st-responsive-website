9d32da43de91c0757d93753e02d8332d585799ec
bhuvan <bhuvanannappa@gmail.com>
2024-09-12 11:52:44 +0530

    added a new feature(The color of the hero element changes when hovered over

--- app.js
+++ app.js
@@ -1,52 +1,49 @@
-
-let main=document.querySelector("#main");
-let cursor=document.querySelector("#cursor");
-let hero=document.querySelectorAll(".overlay");
-let cBgCcolor=document.querySelector(".hvr")
-
- main.addEventListener("mousemove",(e)=>{
-     gsap.to(cursor,{
-         x: e.x, 
-         y: e.y,
-         duration : 1
-     })
- })
+let main = document.querySelector("#main");
+let cursor = document.querySelector("#cursor");
+let hero = document.querySelectorAll(".overlay");
 
 
-gsap.to(".fleftelm", {
-    scrollTrigger: {
-      trigger: ".f-images",
-      pin: true,
-      start: "top top",
-      end: "bottom bottom",
-      endTrigger: ".last",
-      scrub: 1,
-      invalidateOnRefresh: true,
-    },
-    y: "-300%",
-    ease: Power1,
+main.addEventListener("mousemove", (e) => {
+  gsap.to(cursor, {
+    x: e.x,
+    y: e.y,
+    duration: 1
   });
+});
 
 
- hero.forEach((ele)=>{
-    ele.addEventListener("mouseenter",()=>{
-      gsap.to(cursor,{
-        scale:8,
-      })
-    })
- })
+hero.forEach((ele1) => {
+  ele1.addEventListener("mouseenter", (e) => {
+    let element = e.target.getAttribute("id");
+    
+    if (element === "overlay1") {
+      document.querySelector("#c1").style.color = "black";
+    } else if (element === "overlay2") {
+      document.querySelector("#c2").style.color = "black";
+    } else {
+      document.querySelector("#c3").style.color = "black";
+    }
 
- hero.forEach((ele)=>{
-  ele.addEventListener("mouseleave",()=>{
-    cBgCcolor.style.color="";  
-    gsap.to(cursor,{
-        scale:1,
-      })
-  })
-})
+    gsap.to(cursor, {
+      scale: 8
+    });
+  });
+});
 
- 
+hero.forEach((ele2) => {
+  ele2.addEventListener("mouseleave", (e) => {
+    let element = e.target.getAttribute("id");
 
+    if (element === "overlay1") {
+      document.querySelector("#c1").style.color = "transparent";
+    } else if (element === "overlay2") {
+      document.querySelector("#c2").style.color = "transparent";
+    } else {
+      document.querySelector("#c3").style.color = "transparent";
+    }
 
-  
-  
+    gsap.to(cursor, {
+      scale: 1
+    });
+  });
+});
