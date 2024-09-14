f17f7dda9dba17db65df425f942e53f37d64648e
bhuvan <bhuvanannappa@gmail.com>
2024-09-14 16:15:59 +0530

    added the side bar to the website

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ let main = document.querySelector("#main");
 let cursor = document.querySelector("#cursor");
 let hero = document.querySelectorAll(".overlay");
 let Mbtn = document.querySelectorAll(".magnet");
-let Bbtn = document.querySelectorAll(".blackButton")
+let Bbtn = document.querySelectorAll(".blackButton");
+let menu = document.querySelector("#right i");
+let close = document.querySelector("#sidebar i")
 
 
 const videoElement = document.createElement('video');
@@ -13,10 +15,10 @@ videoElement.loop = true;
 videoElement.style.width = "100%";
 videoElement.style.height = "100%";
 videoElement.style.objectFit = "cover";
-videoElement.style.position = "absolute";  
-videoElement.style.top = "0";  
-videoElement.style.left = "0";  
-videoElement.style.zIndex = "1"; 
+videoElement.style.position = "absolute";
+videoElement.style.top = "0";
+videoElement.style.left = "0";
+videoElement.style.zIndex = "1";
 
 
 main.addEventListener("mousemove", (e) => {
@@ -47,22 +49,22 @@ main.addEventListener("mousemove", (e) => {
 hero.forEach((ele1) => {
   ele1.addEventListener("mouseenter", (e) => {
     let element = e.target.getAttribute("id");
-    document.querySelector("#cursor").style.backgroundColor="transparent";
+    document.querySelector("#cursor").style.backgroundColor = "transparent";
     if (element === "overlay1") {
       document.querySelector("#c1").style.color = "black";
-      videoElement.src = "0.mp4"; 
+      videoElement.src = "0.mp4";
     } else if (element === "overlay2") {
       document.querySelector("#c2").style.color = "black";
-      videoElement.src = "2.mp4"; 
+      videoElement.src = "2.mp4";
 
     } else {
       document.querySelector("#c3").style.color = "black";
-      videoElement.src = "3.mp4"; 
+      videoElement.src = "3.mp4";
     }
     cursor.appendChild(videoElement);
     gsap.to(cursor, {
       scale: 10
-      
+
     });
   });
 });
@@ -70,7 +72,7 @@ hero.forEach((ele1) => {
 hero.forEach((ele2) => {
   ele2.addEventListener("mouseleave", (e) => {
     let element = e.target.getAttribute("id");
-    document.querySelector("#cursor").style.backgroundColor="black";
+    document.querySelector("#cursor").style.backgroundColor = "black";
 
     if (element === "overlay1") {
       document.querySelector("#c1").style.color = "transparent";
@@ -86,45 +88,45 @@ hero.forEach((ele2) => {
     while (cursor.firstChild) {
       cursor.removeChild(cursor.firstChild);
     }
-    
+
   });
 });
 
 
-Mbtn.forEach((btn)=>{
-  btn.addEventListener("mousemove",function(e){
-    const position=btn.getBoundingClientRect();
-    const x=e.pageX - position.left - position.width / 2;
-    const y=e.pageY - position.top - position.height / 2;
-    
-    btn.style.transform=`translate(${x*0.3}px,${y*0.5}px)`;
+Mbtn.forEach((btn) => {
+  btn.addEventListener("mousemove", function (e) {
+    const position = btn.getBoundingClientRect();
+    const x = e.pageX - position.left - position.width / 2;
+    const y = e.pageY - position.top - position.height / 2;
+
+    btn.style.transform = `translate(${x * 0.3}px,${y * 0.5}px)`;
   });
 });
 
-Mbtn.forEach((btn)=>{
-  btn.addEventListener("mouseout",function(e){ 
-    btn.style.transform="translate(0px, 0px)";
+Mbtn.forEach((btn) => {
+  btn.addEventListener("mouseout", function (e) {
+    btn.style.transform = "translate(0px, 0px)";
   });
 });
 
-Bbtn.forEach((ele)=>{
-  ele.addEventListener("mouseenter",(e)=>{
+Bbtn.forEach((ele) => {
+  ele.addEventListener("mouseenter", (e) => {
     const val = e.target;
-    val.style.backgroundColor="black";
-    val.style.color="white";
-    cursor.style.opacity = '0'; 
+    val.style.backgroundColor = "black";
+    val.style.color = "white";
+    cursor.style.opacity = '0';
 
 
   })
 })
 
-Bbtn.forEach((ele)=>{
-  ele.addEventListener("mouseout",(e)=>{
+Bbtn.forEach((ele) => {
+  ele.addEventListener("mouseout", (e) => {
     const val = e.target;
-    val.style.backgroundColor="white";
-    val.style.color="black";
-    cursor.style.opacity = '1'; 
-    
+    val.style.backgroundColor = "white";
+    val.style.color = "black";
+    cursor.style.opacity = '1';
+
 
   })
 })
@@ -133,7 +135,7 @@ Bbtn.forEach((ele)=>{
 
 function typeEffect(element, speed) {
   let text = element.innerHTML;
-  element.innerHTML = "";  
+  element.innerHTML = "";
   let i = 0;
 
   let cursor = document.createElement('span');
@@ -142,11 +144,11 @@ function typeEffect(element, speed) {
 
   function type() {
     if (i < text.length) {
-      cursor.insertAdjacentText('beforebegin', text.charAt(i)); 
+      cursor.insertAdjacentText('beforebegin', text.charAt(i));
       i++;
       setTimeout(type, speed);
     }
-    
+
   }
   type();
 }
@@ -155,8 +157,8 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       const element = entry.target.querySelector("p");
-      typeEffect(element, 25);  
-      observer.unobserve(entry.target);  
+      typeEffect(element, 25);
+      observer.unobserve(entry.target);
     }
   });
 });
@@ -165,5 +167,47 @@ window.addEventListener('DOMContentLoaded', () => {
   const homeLast = document.getElementById('home-last');
   observer.observe(homeLast);
 });
+
+var tl = gsap.timeline()
+tl.to("#sidebar", {
+  right: 0,
+  duration: 0.4
+})
+
+tl.from("#sidebar h4", {
+  x: 150,
+  duration: 0.5,
+  stagger: 0.12,
+  opacity: 0,
+})
+
+tl.from("#sidebar i", {
+  opacity: 0
+})
+
+tl.pause()
+
+menu.addEventListener("click", () => {
+  tl.play();
+  let cover = document.createElement("div");
+  cover.className = "cover";
+  document.body.appendChild(cover);
+})
+
+
+close.addEventListener("click", () => {
+  tl.reverse();
+  gsap.to(".cover", {
+    opacity: 0,
+    duration: 1.7,
+    onComplete: () => {
+      const cover = document.querySelector(".cover");
+      if (cover) {
+        cover.remove();
+      }
+    }
+  });
+})
+
 
 
